167549bdc126e499abb4a0fa71dae10e68008684
Josip Matić <jmatic01@fesb.hr>
2018-10-03 16:18:12 +0200

    Adds archive page.

--- public/wp-content/themes/diplomski-rad/archive.php
+++ public/wp-content/themes/diplomski-rad/archive.php
@@ -4,50 +4,26 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package diplomski-rad
+ * @package Diplomski_Rad
  */
 
 get_header();
-?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
+	if ( have_posts() ) :
 		?>
+		<section class="u-pt-16 u-pb-16">
+			<div class="o-container">
+				<div class="o-row">
+				<?php
+				while ( have_posts() ) :
+					the_post();
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+					get_template_part( 'items/visp-example' );
 
-<?php
-get_sidebar();
+				endwhile;
+				?>
+				</div><!-- /.o-row -->
+			</div><!-- /.o-container -->
+		</section>
+		<?php
+	endif;
 get_footer();
