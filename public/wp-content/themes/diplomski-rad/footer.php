40655b3f452c1e150d45fa90bdefa41af0868575
Josip Matić <jmatic01@fesb.hr>
2018-10-03 15:51:57 +0200

    Fixes doc comment.

--- public/wp-content/themes/diplomski-rad/footer.php
+++ public/wp-content/themes/diplomski-rad/footer.php
@@ -7,7 +7,7 @@
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
  * @package WordPress
- * @subpackage Bojler_Site
+ * @subpackage Diplomski_Rad
  * @since 1.0
  * @version 1.2
  */
