b36011944edd43b78014a6a28dfc110e91365a1b
Josip Matić <jmatic01@fesb.hr>
2018-10-03 15:49:19 +0200

    Adds visp example post type.

---
+++ public/wp-content/themes/diplomski-rad/inc/inc.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Includes.
+ *
+ * @package Diplomski_Rad
+ */
+
+require_once trailingslashit( dirname( __FILE__ ) ) . 'media/media.php';
+require_once trailingslashit( dirname( __FILE__ ) ) . 'visp-example/visp-example.php';
