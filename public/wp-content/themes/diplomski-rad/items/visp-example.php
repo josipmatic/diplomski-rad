4dd8808340a8049dbb62fae80518484672a83ef2
Josip Matić <jmatic01@fesb.hr>
2018-10-15 16:36:26 +0200

    Adds image to examples list.

--- public/wp-content/themes/diplomski-rad/items/visp-example.php
+++ public/wp-content/themes/diplomski-rad/items/visp-example.php
@@ -8,10 +8,22 @@
 global $post;
 
 if ( ! empty( $post ) && ! is_wp_error( $post ) ) :
+
+	$thumbnail_url = get_the_post_thumbnail_url( $post, 'full' );
 ?>
 <div class="o-col-4">
-	<h2 class="u-font-normal u-font-bold"><?php echo esc_html( get_the_title() ); ?></h2>
-	<a href="<?php echo esc_url( get_permalink() ); ?>" class="c-btn c-btn--blue c-btn--lg u-h6 u-mr-1 u-mb-2"><?php echo esc_html( 'Go To Example' ); ?></a>
+	<a href="<?php echo esc_url( get_permalink() ); ?>">
+		<h2 class="u-font-normal u-font-bold"><?php echo esc_html( get_the_title() ); ?></h2>
+		<?php
+		if ( ! empty( $thumbnail_url ) ) :
+		?>
+			<img src="<?php echo esc_url( $thumbnail_url ); ?>" />
+		<?php endif; ?>
+		<div class="u-mt-1">
+			<?php the_excerpt(); ?>
+		</div>
+		<a href="<?php echo esc_url( get_permalink() ); ?>" class="c-btn c-btn--blue c-btn--lg u-h6 u-mr-1 u-mb-2"><?php echo esc_html( 'Go To Example' ); ?></a>
+	</a>
 </div><!-- /.o-col -->
 <?php
 endif;
